32215d100275d8f6989135c4346c553f0a8266e1
Ivan <techydad05@gmail.com>
2022-07-03 20:01:02 -0400

    adding home images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,66 +7,28 @@
 	<meta name="description" content="Daveys portfolio site" />
 </svelte:head>
 
-<!-- <section class="bg-contain bg-no-repeat home flex items-center justify-center fixed w-full h-full left-0 top-0">
-	<img class="z-50" src="daveys-logo-2.png" width="90%" alt="">
-</section> -->
-<div class="frame shadow-xl">
-	<img class="hidden md:block" src="background.jpg" alt="daveys favorite" />
-	<img class="md:hidden" src="background-mobile.jpg" alt="daveys favorite" />
-</div>
+<section class="bg-[url('/art-gallery.jpg')] bg-cover bg-no-repeat home flex items-center justify-center fixed w-full h-full left-0 top-0">
+	<div class="w-[70%] absolute top-[180px]">
+		<img src="/background.jpg" alt="Terracotta female statue" class="frame">
+		<img class="z-50 max-w-xl mx-auto" src="daveys-logo-2.png" width="90%" alt="">
+	</div>
+</section>
+
+
 
 <style>
-img {
-  border:solid 2px;
-  border-bottom-color:#ffe;
-  border-left-color:#eed;
-  border-right-color:#eed;
-  border-top-color:#ccb;
-  max-height:100%;
-  max-width:100%;
+section.home {
+	background-position: center top;
 }
-
 .frame {
-	background-color:#fbfbfb;
-	border:solid 5vmin #eee;
-	border-bottom-color: #8c7272;
-	border-left-color: #917777;
-	border-right-color: #917777;
-	border-top-color: #8c7272;
-	box-shadow:0 0 5px 0 rgba(0, 0, 0, 0.338) inset, 0 5px 10px 5px rgba(0,0,0,.338);
-	box-sizing:border-box;
-	display:inline-block;
-	margin: 40px;
-	/* height:90vh; */
-	padding:8vmin;
-	position:relative;
-	text-align:center;
-	display: flex;
-  	align-items: center;
-}
-.frame:before {
-	border-radius:2px;
-	bottom:-2vmin;
-	box-shadow:0 2px 5px 0 rgba(0,0,0,.25) inset;
-	content:"";
-	left:-2vmin;
-	position:absolute;
-	right:-2vmin;
-	top:-2vmin;
-}
-.frame:after {
-	border-radius:2px;
-	bottom:-2.5vmin;
-	box-shadow: 0 2px 5px 0 rgba(0,0,0,.25);
-	content:"";
-	left:-2.5vmin;
-	position:absolute;
-	right:-2.5vmin;
-	top:-2.5vmin;
-}
-@media (min-width: 640px) {
-	.frame {
-		height: 90vh;
-	}
+	border-image: url('/frame-2.png') 80 70 80 70 stretch stretch;
+	border-color: #f4be52;
+	border-style: inset;
+	border-width: 48px;
+	display: block;
+	width: 100%;
+	height: auto;
+	max-width: 500px;
+	margin: 0 auto;
 }
 </style>
